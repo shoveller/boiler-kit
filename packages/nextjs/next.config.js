1091aa465e65c2cbfb8e0763e692db051b6882ed
shoveller <cinos81@gmail.com>
2022-02-06 21:50:19 +0900

    feat: init

---
+++ packages/nextjs/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // static file 압축 활성화
+  compress: true,
+  // react.js의 strict모드 활성화
+  reactStrictMode: true,
+  // eslint의 경고를 무시하지 않음
+  ignoreDuringBuilds: false,
+  // 타입스크립트 빌드 에러를 무시하지 않음
+  ignoreBuildErrors: false,
+  // build indicator 표시 위치를 지정
+  buildActivityPosition: 'bottom-right',
+}
+
+module.exports = nextConfig
