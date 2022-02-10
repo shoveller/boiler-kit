c121b55d95cb43c3a56155c18fc95cec6ad3e4de
SEO JAE WON <yontanine@kakaosign.com>
2022-02-10 17:23:38 +0900

    feat: 테마 제어 예제를 추가

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -1,26 +1,10 @@
 import type { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import '@/styles/globalStyle.css'
-import { defaultTheme } from '../themes/defaultTheme'
-import { darkTheme } from '../themes/darkTheme'
-import { useEffect, useState } from 'react'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const [on, setOn] = useState(true)
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setOn((value) => !value)
-    }, 1000)
-
-    return () => {
-      clearInterval(timer)
-    }
-  }, [])
-
   return (
     <>
-      {on ? <style jsx>{defaultTheme}</style> : <style jsx>{darkTheme}</style>}
-
       {/*eslint-disable-next-line react/jsx-props-no-spreading*/}
       <Component {...pageProps} />
     </>
--- packages/nextjs/pages/theme-example/index.tsx
+++ packages/nextjs/pages/theme-example/index.tsx
@@ -1,7 +1,33 @@
 import { NextPage } from 'next'
+import { Dispatch, FC, SetStateAction, useState } from 'react'
+import { defaultTheme } from '../../themes/defaultTheme'
+import { darkTheme } from '../../themes/darkTheme'
+
+const ThemeChanger: FC<{
+  on: boolean
+  setOn: Dispatch<SetStateAction<boolean>>
+}> = ({ on, setOn }) => {
+  return (
+    <input
+      id="changer"
+      type="checkbox"
+      checked={on}
+      onChange={() => setOn((value) => !value)}
+    />
+  )
+}
 
 const ThemeExample: NextPage = () => {
-  return <p>테마 적용 예제</p>
+  const [on, setOn] = useState(true)
+
+  return (
+    <>
+      <ThemeChanger on={on} setOn={setOn} />
+      {on ? <style jsx>{defaultTheme}</style> : <style jsx>{darkTheme}</style>}
+      <p>테마 적용 예제이며, 프로덕션 코드가 아닙니다.</p>
+      <p>동적으로 css variables를 바꾸는 코드가 핵심입니다.</p>
+    </>
+  )
 }
 
 export default ThemeExample
